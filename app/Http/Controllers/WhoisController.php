22c89c4aa41f41c9c39151263eb4617fa42fecc4
Burt <iamcabigasburtkarl@gmail.com>
2024-08-26 23:38:18 +0800

    adding comments

--- app/Http/Controllers/WhoisController.php
+++ app/Http/Controllers/WhoisController.php
@@ -7,12 +7,24 @@ use Illuminate\Support\Facades\Http;
 
 class WhoisController extends Controller
 {
+    /**
+     * Retrieves WHOIS information for a given domain name.
+     *
+     * - Extracts the domain name from the incoming HTTP request.
+     * - Constructs the WHOIS API request URL using the API key and the provided domain name.
+     * - Sends a GET request to the WHOIS API service, requesting the data in JSON format and ignoring raw texts.
+     * - Returns the JSON response from the WHOIS API.
+     *
+     * @param Request $request The incoming HTTP request containing the domain name.
+     * @return \Illuminate\Http\JsonResponse The JSON response containing WHOIS information.
+     */
     public function getWhois(Request $request)
     {
         $apiKey = env('WHOIS_API_KEY');
+        $apiURL = env('WHOIS_API_URL');
         $domainName = $request->get('domainName');
-        $url = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey={$apiKey}&domainName={$domainName}&outputFormat=JSON&ignoreRawTexts=1";
-        $response = Http::get($url);
+        $requestURL = "{$apiURL}?apiKey={$apiKey}&domainName={$domainName}&outputFormat=JSON&ignoreRawTexts=1";
+        $response = Http::get($requestURL);
 
         return response()->json($response->json());
     }
